7fb709595dd12fd93f3072768bd0df40347a68e1
Nikita Verbilo <nikitaverbilo@icloud.com>
2021-03-01 14:10:55 +0000

    pay updated

--- src/view/pay.js
+++ src/view/pay.js
@@ -1,5 +1,9 @@
 function Pay() {
-  return <section className="pay"></section>;
+  return (
+    <section className="pay">
+      <h3 className="pay-header">Still working on that one!</h3>
+    </section>
+  );
 }
 
 export default Pay;
